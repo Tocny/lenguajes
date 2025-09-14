4d0dd7ca42a107e8761c38656021bb5badae087a
Tocny <marcoarubior@ciencias.unam.mx>
2025-09-14 17:44:55 -0600

    Practica3 stuff

---
+++ practica3/grammars.rkt
@@ -0,0 +1,12 @@
+#lang plai
+;; Definición del tipo Binding
+(define-type Binding
+  [binding (id symbol?) (value FWAE?)])
+
+;; Definición del tipo WAE
+(define-type WAE
+  [id    (i symbol?)]
+  [num   (n number?)]
+  [op    (f procedure?) (args (listof FWAE?))]
+  [with  (bindings (listof binding?)) (body FWAE?)]
+  [with* (bindings (listof binding?)) (body FWAE?)])
