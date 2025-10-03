44e9508a28dc80d455134dd11b09422bf44ad112
Tocny <marcoarubior@ciencias.unam.mx>
2025-10-03 11:35:40 -0600

    Lol

--- practica3/parser.rkt
+++ practica3/parser.rkt
@@ -1,4 +1,12 @@
 #lang plai
+
+;; Practica 3.
+;; Integrantes del Equipo:
+;; - Gonzalez Castillo Patricio Salvador 321142391
+;; - Valencia Pérez Guillermo Emanuel 321018689
+;; - Rubio Resendiz Marco Antonio 320209763
+;; - Sautto Ramirez Seldon 321084163
+
 (require (file "./grammars.rkt"))
 
 ;; parse : s-expression - > FWAE
@@ -10,8 +18,15 @@
      (cond
        [(and (>= (length sexp) 2)
              (member (first sexp) '(+ - * / = modulo expt add1 sub1)))
-        (op (get-op-procedure (first sexp))
-            (map parse (rest sexp)))]
+        (let ([op-symbol (first sexp)]
+              [operands (rest sexp)])
+          ;; Validar aridad para operadores unarios
+          (when (member op-symbol '(add1 sub1))
+            (unless (= (length operands) 1)
+              (error (format "parser: Aridad incorrecta en la función '~a" op-symbol))))
+   
+          (op (get-op-procedure op-symbol)
+              (map parse operands)))]
        [(and (>= (length sexp) 3)
              (eq? (first sexp) 'with))
         (with (parse-bindings (second sexp))
@@ -37,8 +52,20 @@
     [else (error "Syntax Error: Not valid operator")]))
 
 (define (parse-bindings binding-list)
+  ;; Verificar que binding-list sea una lista de listas
+  (unless (and (list? binding-list)
+               (andmap list? binding-list)
+               (andmap (lambda (b) (= (length b) 2)) binding-list))
+    (error "Syntax Error: Invalid binding structure"))
+  
+  ;; Extraer IDs y verificar duplicados
+  (define ids (map first binding-list))
+  (define duplicate (check-duplicates ids))
+  (when duplicate
+    (error (format "parse: El símbolo '~a está declarado más de una vez" duplicate)))
+  
+  ;; Parsear cada binding
   (map (lambda (binding-pair)
-         (if (and (list? binding-pair) (= (length binding-pair) 2))
-             (binding (first binding-pair) (parse (second binding-pair)))
-             (error "Syntax Error: Invalid id assignment ")))
+         (binding (first binding-pair) 
+                  (parse (second binding-pair))))
        binding-list))
